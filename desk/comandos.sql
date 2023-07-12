bcac89831c8a51b4f83850d8b71bffc0dddd8863
matheus_motta <43185269+matheusm22@users.noreply.github.com>
2023-07-12 16:21:45 -0300

    Delete comandos.sql

--- desk/comandos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `pedidos` (
-   `id` int auto_increment primary key,
-  `titulo` varchar(45) NOT NULL,
-  `categoria` varchar(45),
-  `descricao` varchar(100),
-  `dta_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
